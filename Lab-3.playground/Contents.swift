00d7280cc4470511dcd194346e8873aa1ddacaca
Ryan Cornel <ry.cornel@students.makeschool.com>
2020-11-04 20:48:38 -0800

    Question 3 answered.

--- Lab-3.playground/Contents.swift
+++ Lab-3.playground/Contents.swift
@@ -26,4 +26,19 @@ print(counts)
 
 
 // Question #3
+func fibo(n: Int) -> [Int] {
+    
+    assert(n > 1)
+    
+    var array = [0, 1]
+    
+    while array .count < n {
+        
+        array.append(array[array.count - 1] + array[array.count - 2])
+    }
+    
+    
+    return array
+}
 
+print(fibo(n: 15))
